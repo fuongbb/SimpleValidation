3215299ccbdd01d9cd05c28960f8f59cd68cff21
Phuong Ngo <fuongit@gmail.com>
2013-11-04 00:56:37 +0700

    First commit

---
+++ Validation.js
@@ -0,0 +1,75 @@
+var Validation = {
+	email: function(value) {
+		return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
+	},
+	required: function(value) {
+		return $.trim(value).length > 0
+	},
+	minLength: function(value, param) {
+		var length = $.isArray(value) ? value.length : $.trim(value).length;
+		return length >= param;
+	},
+	maxLength: function(value, param) {
+		var length = $.isArray(value) ? value.length : $.trim(value).length;
+		return length <= param;
+	},
+	min: function(value, param) {
+		return value >= param;
+	},
+	max: function(value, param) {
+		return value <= param;
+	},
+	reganeLength: function(value, param) {
+		var length = $.isArray(value) ? value.length : $.trim(value).length;
+		return length >= param[0] && length <= param[1];
+	},
+	regane: function(value, param) {
+		return value >= param[0] && value <= param[1];
+	},
+	url: function(value) {
+		return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
+	},
+	date: function(value) {
+		return !/Invalid|NaN/.test(new Date(value));
+	},
+	dateISO: function(value) {
+		return /^\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}$/.test(value);
+	},
+	number: function(value) {
+		return /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(value);
+	},
+	digits: function(value) {
+		return /^\d+$/.test(value);
+	},
+	creditcard: function(value) {
+		if (/[^0-9 \-]+/.test(value)) {
+			return false;
+		}
+		var nCheck = 0,
+			nDigit = 0,
+			bEven = false;
+
+		value = value.replace(/\D/g, "");
+
+		for (var n = value.length - 1; n >= 0; n--) {
+			var cDigit = value.charAt(n);
+			nDigit = parseInt(cDigit, 10);
+			if (bEven) {
+				if ((nDigit *= 2) > 9) {
+					nDigit -= 9;
+				}
+			}
+			nCheck += nDigit;
+			bEven = !bEven;
+		}
+
+		return (nCheck % 10) === 0;
+	},
+	equalTo: function(value, param) {
+		return value === param;
+	},
+	custom: function(value, param) {
+		var patt = new RegExp(param);
+		return patt.test(value);
+	}
+};
